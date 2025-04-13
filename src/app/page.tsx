28e96c3d05b6a15d795427ad2024fb043f73f016
Mrunu2122 <mrunalsoshte212208@gmail.com>
2025-04-13 15:09:07 +0530

    About section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import React from "react";
 import Home from "@/components/home";
+import About from "@/components/about";
 
 
 const page = () => {
   return (
     <div className="bg-black">
       <Home />
+      <About />
     </div>
   );
 };
